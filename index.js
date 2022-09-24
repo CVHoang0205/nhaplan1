6caf4f613252777ec38b66f0d554a92fa7cd0983
CVHoang0205 <106986621+CVHoang0205@users.noreply.github.com>
2022-09-24 15:12:07 +0700

    a

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require("http");
 http.createServer((req, res) => {
     res.statusCode = 200
     res.setHeader("Content-Type", "text/html");
+    res.write("asdasghd");
     res.end("<h1> Heroku deployment tutorial </h1>");
 })
 .listen(process.env.PORT || 3000);
