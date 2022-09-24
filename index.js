ebd760ada9912968386d50b25a5ca50316f3a987
CVHoang0205 <106986621+CVHoang0205@users.noreply.github.com>
2022-09-24 15:01:52 +0700

    asdgjasd

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require("http");
-http.createServer.((req, res) => {
+http.createServer((req, res) => {
     res.statusCode = 200
     res.setHeader("Content-Type", "text/html");
     res.end("<h1> Heroku deployment tutorial </h1>");
